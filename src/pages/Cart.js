d7e3d004fed0623a124fe464f11a1622de7756dc
Akash-Tandale001 <ak.tandale123@gmail.com>
2022-02-04 14:31:44 +0530

    resposiveness

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -9,7 +9,7 @@ const Cart = () => {
     dispatch(deleteitem(id));
     alert("Item removed to cart succesfully .");
   };
-  
+
   const itemlist = useSelector(selectitemList);
   return (
     <>
@@ -21,27 +21,30 @@ const Cart = () => {
         return (
           <>
             <div className="p-4 m-4 items-center h-90% border-8 shadow-md rounded max-w-80% max-h-80%">
-              <div className="flex-row justify-center">
-                <div className="flex flex-row w-50% justify-between  p-6 rounded-lg shadow-lg bg-white ">
-                  <i
-                    className="fas fa-trash p-4 cursor-pointer"
-                    onClick={() => removeitem(value.id)}
-                  ></i>
-                   <img src={value.imageurl} style={{height:"5rem"}} alt="..."/>
-                  <h5 className="text-gray-900 p-4 text-xl font-medium mb-2">
-                    {value.name}
-                  </h5>
-                  <p className="text-gray-700 text-2xl mb-4">
-                    {value.price}
-                  </p>               
-                  <button
-                    type="button"
-                    className="  p-4 bg-blue-600 text-white font-medium text-xs uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-                  >
-                    Buy Now
-                  </button>
-                </div>
+              {/* <div className="flex flex-col md:flex-col justify-center"> */}
+              <div className="flex flex-col md:flex-row w-50% justify-between  p-6 rounded-lg shadow-lg bg-white ">
+                <i
+                  className="fas fa-trash p-2 cursor-pointer"
+                  onClick={() => removeitem(value.id)}
+                ></i>
+                <img
+                  src={value.imageurl}
+                  className="m-auto md:m-2"
+                  style={{ height: "5rem", width: "5rem" }}
+                  alt="..."
+                />
+                <h5 className="text-gray-900 p-2 text-xl font-medium mb-2">
+                  {value.name}
+                </h5>
+                <p className="text-gray-700 text-2xl mb-4">{value.price}</p>
+                <button
+                  type="button"
+                  className="  p-4 bg-blue-600 text-white font-medium text-xs uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                >
+                  Buy Now
+                </button>
               </div>
+              {/* </div> */}
             </div>
           </>
         );
